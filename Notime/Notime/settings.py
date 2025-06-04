a28c5e0649f3b26bedbf419c0838c1e161a5cd3a
Gwiza Messi Elvis <burnaghost@tuta.io>
2025-06-04 09:56:08 +0300

    Update settings.py

--- Notime/Notime/settings.py
+++ Notime/Notime/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'messi'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['10.12.3.207']
 
 
 # Application definition
